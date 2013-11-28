5a01938371887ea04098abcd7519cb26c2a56df4
Christian Treppo <christian@treppo.org>
2013-11-28 23:16:36 +0000

    Add vcr cassettes to repo after filtering tmdb api key

--- spec/vcr_setup.rb
+++ spec/vcr_setup.rb
@@ -4,4 +4,5 @@ VCR.configure do |c|
   c.ignore_localhost = true
   c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   c.hook_into :faraday
+  c.filter_sensitive_data('<TMDB_API_KEY>') { ENV['TMDB_API_KEY'] }
 end
