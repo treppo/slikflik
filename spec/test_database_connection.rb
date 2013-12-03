ce205fd93a36409739219080d34c161b902231f8
Christian Treppo <christian@treppo.org>
2013-12-03 17:03:30 +0000

    Remove unused test database methods

--- spec/test_database_connection.rb
+++ spec/test_database_connection.rb
@@ -30,28 +30,4 @@ class TestDatabaseConnection
   def teardown_index
     db.drop_node_index 'movies'
   end
-
-  def node_count
-    extract_count(db.execute_query("START n = node(*) RETURN count(*)")) - 1
-  end
-
-  def relationship_count
-    extract_count(db.execute_query("START n = node(*) MATCH (n)-[r]->() RETURN count(*)"))
-  end
-
-  def get_node_property node, property
-    db.get_node_properties(node, [property])[property]
-  end
-
-  def get_relationship_property rel, property
-    db.get_relationship_properties(rel, [property])[property]
-  end
-
-  def find_movies ids
-    ids.map { |id| find_movie id }
-  end
-
-  def extract_count response
-    response['data'].first.first
-  end
 end
