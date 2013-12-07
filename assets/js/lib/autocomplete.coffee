5a62c26a208b39eb1107f379003ed1d7d1339bdb
Christian Treppo <christian@treppo.org>
2013-12-07 21:59:18 +0000

    Implement autosuggest javascript

--- assets/js/lib/autocomplete.coffee
+++ assets/js/lib/autocomplete.coffee
@@ -1,1 +1,26 @@
+$reference1 = $('#reference1')
+$reference2 = $('#reference2')
+$movieId1 = $('#movieId1')
+$movieId2 = $('#movieId2')
 
+renderTemplate = (suggestion) ->
+  """
+    <img src="#{suggestion.poster}">
+    <p>#{suggestion.title}</p>
+  """
+
+typeaheadConfig =
+  remote: 'suggestions.json?title=%QUERY'
+  valueKey: 'title'
+  template: renderTemplate
+
+setIdInputFor = ($input) ->
+  (event, suggestion) ->
+    $input.val suggestion.id
+
+setup = ($reference, $movieId) ->
+  $reference.typeahead typeaheadConfig
+  $reference.on 'typeahead:selected', setIdInputFor $movieId
+
+setup $reference1, $movieId1
+setup $reference2, $movieId2
--- public/lib.js
+++ public/lib.js
@@ -1,5 +1,38 @@
 (function() {
+  var $movieId1, $movieId2, $reference1, $reference2, renderTemplate, setIdInputFor, setup, typeaheadConfig;
 
+  $reference1 = $('#reference1');
+
+  $reference2 = $('#reference2');
+
+  $movieId1 = $('#movieId1');
+
+  $movieId2 = $('#movieId2');
+
+  renderTemplate = function(suggestion) {
+    return "<img src=\"" + suggestion.poster + "\">\n<p>" + suggestion.title + "</p>";
+  };
+
+  typeaheadConfig = {
+    remote: 'suggestions.json?title=%QUERY',
+    valueKey: 'title',
+    template: renderTemplate
+  };
+
+  setIdInputFor = function($input) {
+    return function(event, suggestion) {
+      return $input.val(suggestion.id);
+    };
+  };
+
+  setup = function($reference, $movieId) {
+    $reference.typeahead(typeaheadConfig);
+    return $reference.on('typeahead:selected', setIdInputFor($movieId));
+  };
+
+  setup($reference1, $movieId1);
+
+  setup($reference2, $movieId2);
 
 }).call(this);
 
