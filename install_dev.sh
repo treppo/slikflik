38a48ced00de2c3ef66c4b64e36f09090685dc9a
Christian Treppo <christian@treppo.org>
2015-12-25 16:55:36 +0100

    Update install dev script

--- install_dev.sh
+++ install_dev.sh
@@ -1,4 +1,12 @@
-curl 'http://download.neo4j.org/artifact?edition=community&version=1.9.5&distribution=tarball' -o /tmp/neo4j.tar
-tar -xvf /tmp/neo4j.tar
-cp /tmp/neo4j-community-1.9.5 ./db/test/
-cp /tmp/neo4j-community-1.9.5 ./db/dev/
+#!/bin/sh
+
+VERSION=1.9.9
+
+if [ ! -e neo4j-${VERSION}.tar ]; then
+    curl "http://neo4j.com/artifact.php?name=neo4j-community-${VERSION}-unix.tar.gz" -o neo4j-${VERSION}.tar
+    tar -xvf neo4j-${VERSION}.tar
+fi
+
+mkdir -p db
+cp -R neo4j-community-${VERSION} ./db/test/
+cp -R neo4j-community-${VERSION} ./db/dev/
